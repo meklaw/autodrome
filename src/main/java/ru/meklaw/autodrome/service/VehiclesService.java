c3792b51e274bd8ffcd64b6935ffd95d59c9cedb
Никита Ульянов <nik.ule@yandex.ru>
2023-01-06 17:38:41 +0300

    add enrise methods and update update method

--- src/main/java/ru/meklaw/autodrome/service/VehiclesService.java
+++ src/main/java/ru/meklaw/autodrome/service/VehiclesService.java
@@ -2,24 +2,28 @@ package ru.meklaw.autodrome.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import ru.meklaw.autodrome.models.Enterprise;
+import ru.meklaw.autodrome.dto.VehicleDTO;
 import ru.meklaw.autodrome.models.Manager;
 import ru.meklaw.autodrome.models.Vehicle;
-import ru.meklaw.autodrome.repositories.ManagerRepository;
+import ru.meklaw.autodrome.repositories.EnterprisesRepository;
+import ru.meklaw.autodrome.repositories.VehicleBrandRepository;
 import ru.meklaw.autodrome.repositories.VehiclesRepository;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 @Service
 public class VehiclesService {
     private final VehiclesRepository vehiclesRepository;
+    private final VehicleBrandRepository vehicleBrandRepository;
+    private final EnterprisesRepository enterprisesRepository;
 
     @Autowired
-    public VehiclesService(VehiclesRepository vehiclesRepository) {
+    public VehiclesService(VehiclesRepository vehiclesRepository, VehicleBrandRepository vehicleBrandRepository, EnterprisesRepository enterprisesRepository) {
         this.vehiclesRepository = vehiclesRepository;
+        this.vehicleBrandRepository = vehicleBrandRepository;
+        this.enterprisesRepository = enterprisesRepository;
     }
 
     public List<Vehicle> findAll() {
@@ -38,12 +42,20 @@ public class VehiclesService {
         return vehiclesRepository.findById(id);
     }
 
-
-    public Vehicle update(Vehicle vehicle) {
+    public Vehicle update(long id, Vehicle vehicle) {
+        vehicle.setId(id);
         return vehiclesRepository.save(vehicle);
     }
 
     public void delete(long id) {
         vehiclesRepository.deleteById(id);
+    }
+
+    public void enriseBrand(Vehicle vehicle, VehicleDTO dto) {
+        vehicle.setBrand(vehicleBrandRepository.findById(dto.getVehicleBrandId()).orElseThrow());
+    }
+
+    public void enriseEnterprise(Vehicle vehicle, VehicleDTO dto) {
+        vehicle.setEnterprise(enterprisesRepository.findById(dto.getEnterpriseId()).orElseThrow());
     }
 }
