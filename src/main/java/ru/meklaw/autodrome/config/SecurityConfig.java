436b28e7122ecfdfc95ccd7052e604ff55d6ae2b
Никита Ульянов <nik.ule@yandex.ru>
2023-01-08 16:39:25 +0300

    add correct filterChain

--- src/main/java/ru/meklaw/autodrome/config/SecurityConfig.java
+++ src/main/java/ru/meklaw/autodrome/config/SecurityConfig.java
@@ -3,17 +3,15 @@ package ru.meklaw.autodrome.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import ru.meklaw.autodrome.service.ManageDetailsService;
+
 
 @Configuration
 @EnableWebSecurity
@@ -25,6 +23,25 @@ public class SecurityConfig {
         this.manageDetailsService = manageDetailsService;
     }
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .ignoringRequestMatchers("/api/**")
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers("/**").authenticated()
+                .and()
+                .formLogin();
+
+        http
+                .authorizeHttpRequests()
+                .requestMatchers("/api/**").hasRole("MANAGER")
+                .and()
+                .httpBasic();
+
+        return http.build();
+    }
 
     @Bean
     public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
