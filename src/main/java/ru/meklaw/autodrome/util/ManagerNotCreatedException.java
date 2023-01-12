7e7f260af8f1baa26675b238f87d25b4cf8da407
Никита Ульянов <nik.ule@yandex.ru>
2023-01-12 15:44:05 +0300

    rename to actual auth Entity

--- src/main/java/ru/meklaw/autodrome/util/ManagerNotCreatedException.java
+++
@@ -1,7 +0,0 @@
-package ru.meklaw.autodrome.util;
-
-public class ManagerNotCreatedException extends RuntimeException {
-    public ManagerNotCreatedException(String message) {
-        super(message);
-    }
-}
