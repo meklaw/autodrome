f6fea48f24a48339bef7b9b5eed8830867d4cdcc
Никита Ульянов <nik.ule@yandex.ru>
2023-01-16 01:55:33 +0300

    add rest requests

--- src/main/java/ru/meklaw/autodrome/controllers/view/EnterpriseController.java
+++ src/main/java/ru/meklaw/autodrome/controllers/view/EnterpriseController.java
@@ -1,35 +1,24 @@
 package ru.meklaw.autodrome.controllers.view;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import ru.meklaw.autodrome.models.Person;
-import ru.meklaw.autodrome.service.EnterprisesService;
+import ru.meklaw.autodrome.controllers.rest.EnterpriseRestController;
 
 @Controller
 @RequestMapping("/enterprises")
 public class EnterpriseController {
 
-    private final EnterprisesService enterpriseService;
+    private final EnterpriseRestController enterpriseRestController;
 
-    @Autowired
-    public EnterpriseController(EnterprisesService enterpriseService) {
-        this.enterpriseService = enterpriseService;
+    public EnterpriseController(EnterpriseRestController enterpriseRestController) {
+        this.enterpriseRestController = enterpriseRestController;
     }
 
     @GetMapping
     public String index(Model model) {
-        var authentication = SecurityContextHolder.getContext()
-                                                  .getAuthentication();
-        Person person = (Person) authentication.getPrincipal();
-
-        if (person.getManager() != null)
-            model.addAttribute("enterprises", enterpriseService.findAllByManager());
-        else
-            model.addAttribute("enterprises", enterpriseService.findAll());
+        model.addAttribute("enterprises", enterpriseRestController.index());
 
         return "enterprise/index";
     }
