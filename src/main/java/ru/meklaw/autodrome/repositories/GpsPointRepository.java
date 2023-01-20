614877c99d809004fc44bdad4cc9c6316ff0ff13
Никита Ульянов <nik.ule@yandex.ru>
2023-01-21 02:10:35 +0300

    add findAllById

--- src/main/java/ru/meklaw/autodrome/repositories/GpsPointRepository.java
+++ src/main/java/ru/meklaw/autodrome/repositories/GpsPointRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.meklaw.autodrome.models.GpsPoint;
 
+import java.util.List;
+
 @Repository
 public interface GpsPointRepository extends JpaRepository<GpsPoint, Long> {
+    List<GpsPoint> findAllById(long vehicleId);
 }
