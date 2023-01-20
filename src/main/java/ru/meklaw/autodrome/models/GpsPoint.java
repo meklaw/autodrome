5dd50e2f410f826b6cd581dde487c2c430e695e1
Никита Ульянов <nik.ule@yandex.ru>
2023-01-21 01:38:39 +0300

    add GpsPoint

---
+++ src/main/java/ru/meklaw/autodrome/models/GpsPoint.java
@@ -0,0 +1,36 @@
+package ru.meklaw.autodrome.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.ZonedDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "gps_point")
+public class GpsPoint {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "vehicle_id", referencedColumnName = "id")
+    private Vehicle vehicle;
+
+    @Column(name = "date_time")
+    private ZonedDateTime date_time;
+
+    @Column(name = "x")
+    private double x;
+
+    @Column(name = "y")
+    private double y;
+}
+
