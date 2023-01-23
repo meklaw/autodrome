843fdaa84c79b111a39591d0dcc5c107c2cb4a93
Никита Ульянов <nik.ule@yandex.ru>
2023-01-23 16:02:30 +0300

    set TimeZone as UTC

--- src/main/java/ru/meklaw/autodrome/AutodromeApplication.java
+++ src/main/java/ru/meklaw/autodrome/AutodromeApplication.java
@@ -1,15 +1,23 @@
 package ru.meklaw.autodrome;
 
+import jakarta.annotation.PostConstruct;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class AutodromeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AutodromeApplication.class, args);
+    }
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
     }
 
     @Bean
