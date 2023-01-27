b20304d757d84c616ac41b87481ec8e04e56b057
Никита Ульянов <nik.ule@yandex.ru>
2023-01-27 20:56:18 +0300

    add GpsTripDTO

---
+++ src/main/java/ru/meklaw/autodrome/dto/GpsTripDTO.java
@@ -0,0 +1,21 @@
+package ru.meklaw.autodrome.dto;
+
+import lombok.*;
+import ru.meklaw.autodrome.models.Trip;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GpsTripDTO {
+    List<Trip> trips;
+    ZonedDateTime startDateTime;
+    ZonedDateTime endDateTime;
+    GpsPointDTO startPoint;
+    GpsPointDTO endPoint;
+    String address;
+}
