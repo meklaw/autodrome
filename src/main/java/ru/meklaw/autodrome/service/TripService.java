8588338da992e6f7f69873c7bb24f6ef1cd18950
Никита Ульянов <nik.ule@yandex.ru>
2023-01-28 00:09:09 +0300

    add  findAllTripsDTO

--- src/main/java/ru/meklaw/autodrome/service/TripService.java
+++ src/main/java/ru/meklaw/autodrome/service/TripService.java
@@ -1,10 +1,26 @@
 package ru.meklaw.autodrome.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
+import ru.meklaw.autodrome.dto.FullTripDTO;
+import ru.meklaw.autodrome.dto.GpsPointDTO;
+import ru.meklaw.autodrome.dto.TripDTO;
 import ru.meklaw.autodrome.models.GpsPoint;
+import ru.meklaw.autodrome.models.Trip;
 import ru.meklaw.autodrome.repositories.TripRepository;
+import ru.meklaw.autodrome.repositories.VehiclesRepository;
 
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -13,20 +29,106 @@ import java.util.Optional;
 public class TripService {
     private final TripRepository tripRepository;
     private final GpsPointService gpsPointService;
+    private final VehiclesRepository vehiclesRepository;
+    private final ModelMapper modelMapper;
+    @Value("${yandex.geocoder.api.key}")
+    private String yandexApiKey;
 
     @Autowired
-    public TripService(TripRepository tripRepository, GpsPointService gpsPointService) {
+    public TripService(TripRepository tripRepository,
+                       GpsPointService gpsPointService,
+                       VehiclesRepository vehiclesRepository,
+                       ModelMapper modelMapper) {
         this.tripRepository = tripRepository;
         this.gpsPointService = gpsPointService;
+        this.vehiclesRepository = vehiclesRepository;
+        this.modelMapper = modelMapper;
     }
 
-    public List<GpsPoint> findAllPoints(long vehicleId,
-                                        ZonedDateTime timeStart,
-                                        ZonedDateTime timeEnd) {
+    @Transactional(readOnly = true)
+    public List<GpsPoint> findAllPoints(long vehicleId, ZonedDateTime timeStart, ZonedDateTime timeEnd) {
 
         Optional<ZonedDateTime> begin = tripRepository.findStartTripPoint(vehicleId, timeStart);
         Optional<ZonedDateTime> end = tripRepository.findEndTripPoint(vehicleId, timeEnd);
 
         return gpsPointService.findAll(vehicleId, begin, end);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Trip> findAllTrips(long vehicleId) {
+        return tripRepository.findAllByVehicleId(vehicleId);
+    }
+
+    @Transactional(readOnly = true)
+    public FullTripDTO findAllTripsDTO(long vehicleId, ZonedDateTime startTime, ZonedDateTime endTime) {
+        ZoneId enterpriseTimeZone = vehiclesRepository.findById(vehicleId)
+                                                      .orElseThrow()
+                                                      .getEnterprise()
+                                                      .getTimeZone();
+        List<Trip> trips = tripRepository.findAllByVehicleIdAndStartTimeUtcIsGreaterThanEqualAndEndTimeUtcLessThanEqualOrderByStartTimeUtc(
+                                                 vehicleId,
+                                                 startTime,
+                                                 endTime)
+                                         .stream()
+                                         .peek(trip -> {
+                                             trip.setStartTimeUtc(trip.getStartTimeUtc()
+                                                                      .withZoneSameInstant(enterpriseTimeZone));
+                                             trip.setEndTimeUtc(trip.getEndTimeUtc()
+                                                                    .withZoneSameInstant(enterpriseTimeZone));
+                                         })
+                                         .toList();
+        List<GpsPoint> points = gpsPointService.find(vehicleId,
+                                                       Optional.ofNullable(startTime),
+                                                       Optional.ofNullable(endTime))
+                                               .stream()
+                                               .peek(point -> point.setDateTime(point.getDateTime()
+                                                                                     .withZoneSameInstant(
+                                                                                             enterpriseTimeZone)))
+                                               .toList();
+
+
+        return FullTripDTO.builder()
+                          .trips(trips.stream()
+                                      .map(TripDTO::new)
+                                      .toList())
+                          .startDateTime(trips.get(0)
+                                              .getStartTimeUtc())
+                          .endDateTime(trips.get(trips.size() - 1)
+                                            .getEndTimeUtc())
+                          .startPoint(convertToGpsPointDTO(points.get(0)))
+                          .endPoint(convertToGpsPointDTO(points.get(points.size() - 1)))
+                          .build();
+    }
+
+    @NotNull
+    private String findAddress(GpsPoint point) {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "https://geocode-maps.yandex.ru/1.x/?apikey=" + yandexApiKey + "&format=json&geocode=" + point.getX() + "," + point.getY();
+        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            JsonNode root = mapper.readTree(response.getBody());
+            return root.get("response")
+                       .get("GeoObjectCollection")
+                       .get("featureMember")
+                       .get(0)
+                       .get("GeoObject")
+                       .get("metaDataProperty")
+                       .get("GeocoderMetaData")
+                       .get("text")
+                       .asText();
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @NotNull
+    @Contract(pure = true)
+    private GpsPointDTO convertToGpsPointDTO(GpsPoint point) {
+        GpsPointDTO gpsPointDTO = modelMapper.map(point, GpsPointDTO.class);
+        gpsPointDTO.setAddress(findAddress(point));
+
+        return gpsPointDTO;
     }
 }
