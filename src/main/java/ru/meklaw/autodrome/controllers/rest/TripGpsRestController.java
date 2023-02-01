29982df27445d4104c683307086f49d0dfdc30e7
Никита Ульянов <nik.ule@yandex.ru>
2023-02-01 23:17:12 +0300

    add indexTrip and generateTrip

--- src/main/java/ru/meklaw/autodrome/controllers/rest/TripGpsRestController.java
+++ src/main/java/ru/meklaw/autodrome/controllers/rest/TripGpsRestController.java
@@ -1,8 +1,11 @@
 package ru.meklaw.autodrome.controllers.rest;
 
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.meklaw.autodrome.dto.FullTripDTO;
+import ru.meklaw.autodrome.dto.GenerateTrip;
 import ru.meklaw.autodrome.dto.GpsPointDTO;
 import ru.meklaw.autodrome.dto.TripDTO;
 import ru.meklaw.autodrome.service.TripGpsService;
@@ -38,8 +41,13 @@ public class TripGpsRestController {
                              .map(tripGpsService::convertToTripDTO)
                              .toList();
     }
+    @GetMapping("/{tripId}")
+    public TripDTO indexTrip(@PathVariable() long tripId) {
+        return tripGpsService.convertToTripDTO(tripGpsService.findTrip(tripId));
+    }
 
-    @GetMapping("/point")
+
+    @GetMapping("/points")
     public Object indexTripPoints(@RequestParam() long vehicle_id,
                                   @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) ZonedDateTime time_start,
                                   @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) ZonedDateTime time_end,
@@ -55,7 +63,7 @@ public class TripGpsRestController {
         return gpsPoints;
     }
 
-    @GetMapping("/point/{tripId}")
+    @GetMapping("/{tripId}/points")
     public List<GpsPointDTO> indexTripPoints(@PathVariable() long tripId) {
         return tripGpsService.findAllTripPoints(tripId)
                              .stream()
@@ -63,9 +71,18 @@ public class TripGpsRestController {
                              .collect(Collectors.toList());
     }
 
-    @GetMapping("/map/{tripId}")
+    @GetMapping("/{tripId}/map")
     public String indexTripMap(@PathVariable() long tripId) {
-        return tripGpsService.findMapForTrip(tripId);
+        return tripGpsService.getMapUrlForTrip(tripId);
     }
 
+    @PostMapping
+    public ResponseEntity<HttpStatus> generateTrip(@RequestBody GenerateTrip generateTrip) {
+        if (generateTrip.getLengthKm() > 1500) {
+            throw new RuntimeException("The route length is too long. The length of the route must be less than 1500 km.");
+        }
+        tripGpsService.generateTrip(generateTrip);
+
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
 }
