639761db81997627db7774c51566c74b81e831c2
Никита Ульянов <nik.ule@yandex.ru>
2023-02-12 02:07:22 +0300

    update dump

--- DB/smallData/InitWithOutGpsData.sql
+++ DB/smallData/InitWithOutGpsData.sql
@@ -5,27 +5,6 @@
 -- Dumped from database version 14.5
 -- Dumped by pg_dump version 14.5
 
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
---
--- Name: autodrome; Type: DATABASE; Schema: -; Owner: postgres
---
-
-CREATE DATABASE autodrome WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE = 'Russian_Russia.1251';
-
-
-ALTER DATABASE autodrome OWNER TO postgres;
-
-\connect autodrome
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
