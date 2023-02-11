ab3709f673482a0f99c0be8c27d73a895b6d02b1
Никита Ульянов <nik.ule@yandex.ru>
2023-02-11 03:43:23 +0300

    add toString

--- src/main/java/ru/meklaw/autodrome/dto/Report.java
+++ src/main/java/ru/meklaw/autodrome/dto/Report.java
@@ -1,9 +1,6 @@
 package ru.meklaw.autodrome.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
@@ -11,6 +8,7 @@ import java.util.TreeMap;
 
 @Getter
 @Setter
+@ToString
 @AllArgsConstructor
 @NoArgsConstructor
 public class Report {
